fd6009ada13e44a27108017c6d023b260e338ebf
trike576 <kunalkanse2006@gmail.com>
2025-08-06 01:42:27 +0530

    Update Recursive_factorial.cpp
    I had made an mistake in this program so I fixed it.

--- Recursive_factorial.cpp
+++ Recursive_factorial.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 using namespace std;
-void fact(int num){
+int fact(int num){
     int factorial=1;
-    if(num<0){
-        cout<<"Invalid"<<endl;
+    if(num==0){
+        return 1;
     }
-    else if(num==1){
-        cout<<"1"<<endl;
-    }
-    else {
-        for(int i=2;i<=num;i++){
-            factorial*=i;
-        }
-            cout<<"The factorial of "<<num<<" is "<<factorial<<endl;
+    else{
+        return num*fact(num-1);
     }
 }
 int main(){
     int num;
     cout<<"Enter an number: ";
     cin>>num;
-    fact(num);
+    if(num<0){
+        cout<<"Invalid"<<endl;
+    }
+    else
+    {
+    cout<<"The factorial of "<<num<<" is "<<fact(num)<<endl;
+    }
     return 0;
 }
